eccf73d8c46da77f85165e54aac891f1b2ce83e1
Igor Santos <igorcost10@gmail.com>
2022-04-10 11:26:53 -0300

    refactor: (wip) remove variables and comments

--- src/index.js
+++ src/index.js
@@ -2,17 +2,12 @@ const fs = require('fs');
 const readline = require('readline');
 const { google } = require('googleapis');
 
-// If modifying these scopes, delete token.json.
 const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
 const TOKEN_PATH = 'token.json';
 
-// Load client secrets from a local file.
 fs.readFile('credentials.json', (err, content) => {
     if (err) return console.log('Error loading client secret file:', err);
-    // Authorize a client with credentials, then call the Gmail API.
+
     authorize(JSON.parse(content), countsEmailsSenders);
 });
 
@@ -24,12 +19,11 @@ fs.readFile('credentials.json', (err, content) => {
  */
 function authorize(credentials, callback) {
     const { client_secret, client_id, redirect_uris } = credentials.installed;
-    const oAuth2Client = new google.auth.OAuth2(
-        client_id, client_secret, redirect_uris[0]);
+    const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
-    // Check if we have previously stored a token.
     fs.readFile(TOKEN_PATH, (err, token) => {
         if (err) return getNewToken(oAuth2Client, callback);
+
         oAuth2Client.setCredentials(JSON.parse(token));
         callback(oAuth2Client);
     });
@@ -46,17 +40,19 @@ function getNewToken(oAuth2Client, callback) {
         access_type: 'offline',
         scope: SCOPES,
     });
+
     console.log('Authorize this app by visiting this url:', authUrl);
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
     });
+
     rl.question('Enter the code from that page here: ', (code) => {
         rl.close();
         oAuth2Client.getToken(code, (err, token) => {
             if (err) return console.error('Error retrieving access token', err);
+
             oAuth2Client.setCredentials(token);
-            // Store the token to disk for later program executions
             fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
                 if (err) return console.error(err);
                 console.log('Token stored to', TOKEN_PATH);
@@ -84,7 +80,7 @@ function countsEmailsSenders(auth) {
         const messages = res.data.messages;
 
         try {
-            const mailCounter = messages.map(message => {
+            messages.map(message => {
                 gmail.users.messages.get({ userId: "me", id: message.id }, async (err, message) => {
 
                     if (err)
